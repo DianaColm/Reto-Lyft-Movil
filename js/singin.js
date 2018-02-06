312abd080f416a2bba9c5c476b59e4e25e88ae84
DianaColm <dcolmenerog@yahoo.com.mx>
2018-02-06 17:08:27 -0600

    subiendo pantalla con banderitas

---
+++ js/singin.js
@@ -0,0 +1,39 @@
+/*plugin de banderas*/
+$(document).ready(function () {
+    $('#phone').intlTelInput();
+});
+
+/*se declara el valor del boton next */
+var next = $('.next');
+/*el botón estraa desactivado hasta que se introduzca un numero de 10 digitos */
+$("#phone").keypress(function () {
+    if ((this.value).length === 9) {
+        next.removeClass('disabled');
+    } else if ((this.value).length !== 10) {
+        next.addClass('disabled');
+    }
+});
+
+/*función para crear código random*/
+function makeid() {
+    var text = "";
+    var possible = "0123456789";
+    for (var i = 0; i < 5; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+};
+
+/*Se genera código y se muestra en un alert*/
+next.on("click", function (event) {
+    event.preventDefault();
+    makeCode();
+    alert('Tu código: ' + 'LAB-' + code);
+    window.location.href = 'form.html';
+});
+
+var code ;
+function makeCode() {
+  code= makeid();
+  localStorage.saving = code;
+}
